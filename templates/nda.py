7ca580e918e2c5561f99c040393b71b74d41ed10
YannickWidmer <yannick.widmer@math.uzh.ch>
2019-03-15 15:54:37 -0700

    Fix #4

--- templates/nda.py
+++ templates/nda.py
@@ -1,7 +1,8 @@
 
 from num2words import num2words
 
-expected_values = ['ownerName', 'recipientName', 'contractEndWithinDays', 'isEffectiveDateSpecific', 'isDisclosurePerpetual', 'lawState','isOwnerCompany', 'isRecipientCompany', 'ownerAddress', 'ownerCity', 'ownerState', 'ownerZipCode', 'recipientAddress', 'recipientCity','recipientState', 'recipientZipCode']
+expected_values = ['ownerName', 'recipientName', 'contractEndWithinDays', 'isEffectiveDateSpecific', 'isDisclosurePerpetual', 'lawState','isOwnerCompany', 'isRecipientCompany',
+ 'ownerAddress', 'ownerCity', 'ownerState', 'ownerZipCode', 'recipientAddress', 'recipientCity','recipientState', 'recipientZipCode', 'agreementExpireInYears']
 additional = ['contractDated','disclosureExpireInYears','ownerRepresentantName', 'ownerRepresentantTitle', 'recipientRepresentantName', 'recipientRepresentantTitle']
 
 html_parameters = { # Parameters that can be set with their standard value
@@ -11,6 +12,60 @@ html_parameters = { # Parameters that can be set with their standard value
     'isEffectiveDateSpecific' : False
 }
 html_values = ['ownerName', 'recipientName', 'contractDated', 'contractEndWithinDays', 'lawState', 'ownerAddress', 'ownerCity', 'ownerState', 'ownerZipCode', 'recipientAddress', 'recipientCity','recipientState', 'recipientZipCode', 'ownerRepresentantName', 'ownerRepresentantTitle', 'recipientRepresentantName', 'recipientRepresentantTitle', 'disclosureExpireInYears']
+
+# TO get state initials
+us_state_abbrev = {
+    'Alabama': 'AL',
+    'Alaska': 'AK',
+    'Arizona': 'AZ',
+    'Arkansas': 'AR',
+    'California': 'CA',
+    'Colorado': 'CO',
+    'Connecticut': 'CT',
+    'Delaware': 'DE',
+    'Florida': 'FL',
+    'Georgia': 'GA',
+    'Hawaii': 'HI',
+    'Idaho': 'ID',
+    'Illinois': 'IL',
+    'Indiana': 'IN',
+    'Iowa': 'IA',
+    'Kansas': 'KS',
+    'Kentucky': 'KY',
+    'Louisiana': 'LA',
+    'Maine': 'ME',
+    'Maryland': 'MD',
+    'Massachusetts': 'MA',
+    'Michigan': 'MI',
+    'Minnesota': 'MN',
+    'Mississippi': 'MS',
+    'Missouri': 'MO',
+    'Montana': 'MT',
+    'Nebraska': 'NE',
+    'Nevada': 'NV',
+    'New Hampshire': 'NH',
+    'New Jersey': 'NJ',
+    'New Mexico': 'NM',
+    'New York': 'NY',
+    'North Carolina': 'NC',
+    'North Dakota': 'ND',
+    'Ohio': 'OH',
+    'Oklahoma': 'OK',
+    'Oregon': 'OR',
+    'Pennsylvania': 'PA',
+    'Rhode Island': 'RI',
+    'South Carolina': 'SC',
+    'South Dakota': 'SD',
+    'Tennessee': 'TN',
+    'Texas': 'TX',
+    'Utah': 'UT',
+    'Vermont': 'VT',
+    'Virginia': 'VA',
+    'Washington': 'WA',
+    'West Virginia': 'WV',
+    'Wisconsin': 'WI',
+    'Wyoming': 'WY',
+}
 
 
 def render(data, to_pdf):
@@ -41,6 +96,10 @@ def render(data, to_pdf):
         if len(error) >0 :
             raise ValueError(error)
 
+            # Setting State initials
+        for state in ['ownerState', 'recipientState']:
+            data[state+'Initials'] = us_state_abbrev[data[state]]
+
         # Creating word version of contractEndWithinDays
         data['contractEndWithinDaysWord'] = num2words(data['contractEndWithinDays'])
 
@@ -55,8 +114,8 @@ def render(data, to_pdf):
             data[key] = f"\$details.{key}"
 
     # Computing isCompany
-
     data['isCompany'] =  ('Yes' if data['isOwnerCompany'] else 'No') + ('Yes' if data['isRecipientCompany'] else 'No')
+
 
     # Passing a copy of to_pdf to the Jinja
     data['toPdf'] = to_pdf
